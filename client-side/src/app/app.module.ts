bbbb81dea689e8fa32d0b6a9c26b6d1645710c48
Razvan Vasile <razvanflorian.vasile@studio.unibo.it>
2023-08-14 11:18:20 +0200

    deps: add angular material

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule // TODO RV perhaps import in another module
+    HttpClientModule,
+    BrowserAnimationsModule, // TODO RV perhaps import in another module
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
