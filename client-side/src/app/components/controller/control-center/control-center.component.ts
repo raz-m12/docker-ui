6beea0a7806df1fb13defebcbf9c467e50bab4c1
Razvan Vasile <razvanflorian.vasile@studio.unibo.it>
2023-08-23 11:27:28 +0200

    feat(control-center): mock-up for the different type of actions a container may do

---
+++ client-side/src/app/components/controller/control-center/control-center.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-center',
+  templateUrl: './control-center.component.html',
+  styleUrls: ['./control-center.component.scss']
+})
+export class ControlCenterComponent {
+
+  build() {
+
+  }
+
+  stop() {
+
+  }
+
+  start() {
+
+  }
+
+  restart() {
+
+  }
+
+  delete() {
+
+  }
+
+  logs() {
+
+  }
+}
